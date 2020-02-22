ab457ca6acaba7cf89d2bb4827606cf51a72d7b6
alexander.dunn@outlook.com <alexander.dunn@outlook.com>
2020-02-22 16:24:01 -0500

    Initial project

---
+++ src/Xamarin.Yeet/Xamarin.Yeet/ViewExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xamarin.Yeet
+{
+    public static class ViewExtensions
+    {
+        public static void Yeet(this View view)
+        {
+            view?.TranslateTo(9000, 9000, 2000);
+        }
+    }
+}
