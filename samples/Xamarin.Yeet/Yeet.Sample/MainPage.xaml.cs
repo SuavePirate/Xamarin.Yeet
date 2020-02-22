13d1d524e5195b3ddd09378d768fbf4d028d2540
alexander.dunn@outlook.com <alexander.dunn@outlook.com>
2020-02-22 16:32:13 -0500

    sample project

---
+++ samples/Xamarin.Yeet/Yeet.Sample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Yeet;
+
+namespace Yeet.Sample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Yeet();
+        }
+
+        void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+        {
+            (sender as View)?.Yeet();
+        }
+    }
+}
